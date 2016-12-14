3fe799d2cd4b2916b9ca6838bc79e911e2b0d33e
klaxalk <Tomas Baca>
2016-12-14 20:33:31 +0100

    made progress

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -17,6 +17,27 @@ if [[ $response =~ ^(yes|y| ) ]]; then
   ln -s $APP_PATH/dotvimrc ~/.vimrc
   ln -s $APP_PATH/dotvim ~/.vim
 
+  # add variable for ctags sources into .bashrc
+  num=`cat ~/.bashrc | grep "CTAGS_SOURCE_DIR" | wc -l`
+  if [ "$num" -lt "1" ]; then
+  
+    echo "Adding CTAGS_SOURCE_DIR variable to .bashrc"
+    # set bashrc
+    echo '
+# where should ctags look for sourcs to parse?
+# -R dir1 -R dir2 ...
+export CTAGS_SOURCE_DIR="-R ~/ros_workspace"' >> ~/.bashrc
+ 
+  fi
+
+  # check whether ~/.my.vimrc file exists, copy mine version if does not 
+  if [ ! -f ~/.my.vimrc ]; then
+    echo "Creating ~/.my.vimrc file"
+      
+    cp $APP_PATH/dotmy.vimrc ~/.my.vimrc
+
+  fi
+  
   vim -E +PluginInstall +qall
 
 fi
