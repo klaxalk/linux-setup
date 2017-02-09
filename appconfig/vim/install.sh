9d8e4f7c73545f1e31aa317e323f29a5b35b0f0a
Tomas Baca <klaxalk@gmail.com>
2017-02-09 18:51:14 +0100

    updating ... updating ...

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -19,8 +19,6 @@ if [[ $response =~ ^(yes|y| ) ]]; then
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
             --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
