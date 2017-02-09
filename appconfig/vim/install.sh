7ce011da1b1e356dc6749e36c372f3e93a40274a
Tomas Baca <klaxalk@gmail.com>
2017-02-09 18:55:41 +0100

    updated install.sh

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -12,16 +12,15 @@ if [[ $response =~ ^(yes|y| ) ]]; then
   toilet Setting up vim
 
   # compile vim from sources
-  toilet Compiling vim
   cd $APP_PATH/../../submodules/vim
   ./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-perlinterp=yes \
-            --enable-luainterp=yes \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+    --enable-multibyte \
+    --enable-rubyinterp=yes \
+    --enable-pythoninterp=yes \
+    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+    --enable-perlinterp=yes \
+    --enable-luainterp=yes \
+    --enable-gui=gtk2 --enable-cscope --prefix=/usr
   cd src
   make
   cd ../
@@ -37,43 +36,51 @@ if [[ $response =~ ^(yes|y| ) ]]; then
   # add variable for ctags sources into .bashrc
   num=`cat ~/.bashrc | grep "CTAGS_SOURCE_DIR" | wc -l`
   if [ "$num" -lt "1" ]; then
-  
+
     echo "Adding CTAGS_SOURCE_DIR variable to .bashrc"
     # set bashrc
     echo '
-# where should ctags look for sources to parse?
-# -R dir1 -R dir2 ...
-export CTAGS_SOURCE_DIR="-R ~/ros_workspace"' >> ~/.bashrc
- 
+    # where should ctags look for sources to parse?
+    # -R dir1 -R dir2 ...
+    export CTAGS_SOURCE_DIR="-R ~/ros_workspace"' >> ~/.bashrc
+
   fi
 
   # add variable for ctags sources into .bashrc
   num=`cat ~/.bashrc | grep "CTAGS_ONCE_SOURCE_DIR" | wc -l`
   if [ "$num" -lt "1" ]; then
-  
+
     echo "Adding CTAGS_ONCE_SOURCE_DIR variable to .bashrc"
     # set bashrc
     echo '
-# where should ctags look for sources to parse?
-# CTAGS FROM THOSE FILE WILL BE CREATED ONLY ONCE
-# -R dir1 -R dir2 ...
-export CTAGS_ONCE_SOURCE_DIR="-R /opt/ros/indigo/include"' >> ~/.bashrc
- 
+    # where should ctags look for sources to parse?
+    # CTAGS FROM THOSE FILE WILL BE CREATED ONLY ONCE
+    # -R dir1 -R dir2 ...
+    export CTAGS_ONCE_SOURCE_DIR="-R /opt/ros/indigo/include"' >> ~/.bashrc
+
   fi
 
   # check whether ~/.my.vimrc file exists, copy mine version if does not 
   if [ ! -f ~/.my.vimrc ]; then
     echo "Creating ~/.my.vimrc file"
-      
+
     cp $APP_PATH/dotmy.vimrc ~/.my.vimrc
 
   fi
-  
+
   vim -E +PluginInstall +qall
 
   # set youcompleteme
   toilet Setting up youcompleteme
-  cd ~/.vim/VundlePlugins/youcompleteme/
-  ./install.py --all
+
+  read -r -p $'\033[31mCompile YouCompleteMe? [y/n] \033[00m' response
+
+  response=${response,,} # tolower
+  if [[ $response =~ ^(yes|y| ) ]]; then
+
+    cd ~/.vim/VundlePlugins/youcompleteme/
+    ./install.py --all
+
+  fi
 
 fi
