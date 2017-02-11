a64f78d13dd87358370e855241efac44dfbcd67d
klaxalk <Tomas Baca>
2017-02-11 16:38:37 +0100

    youcompleteme finaly works

--- appconfig/vim/dotycm_extra_conf.py
+++ appconfig/vim/dotycm_extra_conf.py
@@ -59,19 +59,19 @@ def GetRosIncludeFlags():
 default_flags = [
     '-Wall',
     '-Wextra',
-    '-Werror',
-    '-Wc++98-compat',
+    # '-Werror',
     '-Wno-long-long',
     '-Wno-variadic-macros',
     '-fexceptions',
     '-DNDEBUG',
+    '-DUSE_CLANG_COMPLETER',
     # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know
     # which language to use when compiling headers. So it will guess. Badly. So
     # C++ headers will be compiled as C headers. You don't want that so ALWAYS
     # specify a "-std=<something>".
     # For a C project, you would set this to something like 'c99' instead of
-    'c++11',
-    '-std=c++03',
+    '-std=c++11',
+    # '-std=libc++',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
@@ -84,6 +84,11 @@ default_flags = [
     # include third party libraries
     # '-isystem',
     # '/some/path/include',
+
+    '-isystem',
+    '/usr/include/',
+    '-isystem',
+    '/usr/lib/',
 ]
 
 flags = default_flags + GetRosIncludeFlags()
