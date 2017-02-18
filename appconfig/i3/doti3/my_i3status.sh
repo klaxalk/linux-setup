47858805463496f225dc61d58537f611d394ae64
Tomas Baca <klaxalk@gmail.com>
2017-02-18 11:58:23 +0100

    tweaking i3

--- appconfig/i3/doti3/my_i3status.sh
+++ appconfig/i3/doti3/my_i3status.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # shell script to prepend i3status with more stuff
-
-source ~/.bashrc
 
 i3status -c ~/.i3/i3status.conf | while :
 do
         read line
+
+        source /home/klaxalk/.i3/env_variables.txt
+
         echo '$ROS_IP='"$ROS_IP"' | $UAV_NAME='"$UAV_NAME $line" || exit 1
 done
