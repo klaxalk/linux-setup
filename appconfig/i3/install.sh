f9514bc54714f378f34a386bf9b03f0504c521fe
Tomas Baca <klaxalk@gmail.com>
2017-04-10 16:55:33 +0200

    updated for 16.04

--- appconfig/i3/install.sh
+++ appconfig/i3/install.sh
@@ -35,41 +35,23 @@ if [[ $response =~ ^(yes|y| ) ]]; then
   ln -s $APP_PATH/gtk/settings.ini ~/.config/gtk-3.0/
 
   # install thunar
-  sudo apt-get install thunar gnome-icon-theme-full
+  sudo apt-get install thunar gnome-icon-theme rofi compton i3blocks systemd
 
-  # install arc gtk theme if not installed
-  if [ ! -f /etc/apt/sources.list.d/arc-theme.list ]; then
+  ## install arc gtk theme if not installed
+  #if [ ! -f /etc/apt/sources.list.d/arc-theme.list ]; then
 
-    wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
-    sudo apt-key add - < Release.key
-    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
-    sudo apt-get update
-    sudo apt-get install arc-theme
-    
-  fi
+  #  wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
+  #  sudo apt-key add - < Release.key
+  #  sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+  #  sudo apt-get update
+  #  sudo apt-get install arc-theme
+  #  
+  #fi
 
-  # install moka icon theme
-  sudo add-apt-repository ppa:moka/daily
-  sudo apt-get update
-  sudo apt-get install moka-icon-theme
-
-  # install rofi
-  cd /tmp
-  wget https://launchpad.net/ubuntu/+source/rofi/0.15.11-1/+build/8289001/+files/rofi_0.15.11-1_amd64.deb
-  sudo dpkg -i rofi_0.15.11-1_amd64.deb
-  cd $APP_PATH
-
-  # install compton
-  sudo apt-get install compton
-
-  # install i3blocks
-  cd /tmp
-  wget https://launchpad.net/ubuntu/+source/i3blocks/1.4-1/+build/7637635/+files/i3blocks_1.4-1_amd64.deb
-  sudo dpkg -i i3blocks_1.4-1_amd64.deb
-  cd $APP_PATH
-
-  # install shutdown menu for i3blocks
-  sudo apt-get install systemd
+  ## install moka icon theme
+  #sudo add-apt-repository ppa:moka/daily
+  #sudo apt-get update
+  #sudo apt-get install moka-icon-theme
 
   # put $USE_I3 into bashrc
   num=`cat ~/.bashrc | grep "USE_I3" | wc -l`
