54f5cad1c026fd1157319e2787f8458bc79801b1
Tomas Baca <klaxalk@gmail.com>
2017-05-17 10:40:13 +0200

    update

--- appconfig/i3/doti3/toggle_touchpad.sh
+++ appconfig/i3/doti3/toggle_touchpad.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# author: Tomas Baca
 
 # run "xinput" to get this id, e.g. from the line:
 # ↳ DLL075B:01 06CB:76AF Touchpad             id=13   [slave  pointer  (2)]
@@ -11,9 +12,16 @@ echo $DEVICE_NUMBER
 
 if xinput list-props "$DEVICE_NUMBER" | grep "Device Enabled (137):.1" >/dev/null
 then
+
   xinput disable "$DEVICE_NUMBER"
   notify-send -u low -i mouse "Touchpad disabled"
+
+  # banish mouse
+  xdotool mousemove 10000 10000
+
 else
+
   xinput enable "$DEVICE_NUMBER"
   notify-send -u low -i mouse "Touchpad enabled"
+
 fi
