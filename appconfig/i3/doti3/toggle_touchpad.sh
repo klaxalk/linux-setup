ddfee2b4d4f0d72692e678421f56117a0afa4134
Tomas Baca <klaxalk@gmail.com>
2017-05-17 10:16:26 +0200

    updated latex snippets

--- appconfig/i3/doti3/toggle_touchpad.sh
+++ appconfig/i3/doti3/toggle_touchpad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if xinput list-props 10 | grep "Device Enabled (137):.1" >/dev/null
+if xinput list-props 13 | grep "Device Enabled (137):.1" >/dev/null
 then
-  xinput disable 10
+  xinput disable 13
   notify-send -u low -i mouse "Trackpad disabled"
 else
-  xinput enable 10
+  xinput enable 13
   notify-send -u low -i mouse "Trackpad enabled"
 fi
