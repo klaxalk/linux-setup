edd867efaac7a94c756e4c21a74fcbd578fdd925
Tomáš Báča <bacatoma@fel.cvut.cz>
2017-05-22 14:32:13 +0200

    updated install scripts

--- appconfig/tmux/install.sh
+++ appconfig/tmux/install.sh
@@ -4,39 +4,47 @@
 APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
 
-resp=y
-[[ -t 0 ]] && {
-read -t 10 -n 1 -p $'\033[31mInstall TMUX? [y/n] \033[00m' resp || resp=y ; }
-if [[ $resp =~ ^(y|Y|)$ ]]
-then
+while true; do
+  [[ -t 0 ]] && { read -t 10 -n 2 -p $'\033[31mInstall TMUX? [y/n] \033[00m' resp || resp="y" ; }
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-  toilet Installing tmux
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
 
-  libevent=`ldconfig -p | grep libevent-2.0 | wc -l`
-  if [ "$libevent" -lt "2" ]; then
+    toilet Installing tmux
 
-    echo $'\033[31mLibevent not installed, installing... \033[00m' 
+    libevent=`ldconfig -p | grep libevent-2.0 | wc -l`
+    if [ "$libevent" -lt "2" ]; then
 
-    # install libevent
-    cd /tmp
-    wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
-    tar -xvf libevent-2.0.22-stable.tar.gz
-    rm libevent-2.0.22-stable.tar.gz
-    cd libevent-2.0.22-stable
-    ./configure && make >> /dev/null
-    sudo make install
+      echo $'\033[31mLibevent not installed, installing... \033[00m' 
 
+      # install libevent
+      cd /tmp
+      wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+      tar -xvf libevent-2.0.22-stable.tar.gz
+      rm libevent-2.0.22-stable.tar.gz
+      cd libevent-2.0.22-stable
+      ./configure && make >> /dev/null
+      sudo make install
+
+    fi
+
+    # instal tmux
+    cd $APP_PATH/../../submodules/tmux
+    sh autogen.sh
+    ./configure && make -j4
+    sudo make install-binPROGRAMS
+    git clean -fd
+
+    # symlink tmux settings
+    rm ~/.tmux.conf
+    ln -s $APP_PATH/dottmux.conf ~/.tmux.conf
+
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
   fi
-  
-  # instal tmux
-  cd $APP_PATH/../../submodules/tmux
-  sh autogen.sh
-  ./configure && make -j4
-  sudo make install-binPROGRAMS
-  git clean -fd
-
-  # symlink tmux settings
-  rm ~/.tmux.conf
-  ln -s $APP_PATH/dottmux.conf ~/.tmux.conf
-
-fi
+done
