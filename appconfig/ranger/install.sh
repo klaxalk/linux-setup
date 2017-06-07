e73bab477fda65cb64f4ede135fb78c8c0162f4a
Tomáš Báča <bacatoma@fel.cvut.cz>
2017-06-07 12:49:12 +0200

    extended config for ranger'

--- appconfig/ranger/install.sh
+++ appconfig/ranger/install.sh
@@ -4,6 +4,16 @@
 APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
 
+# img2txt
+sudo apt -y install caca-utils libimage-exiftool-perl
+
 # symlink vim settings
 rm ~/.config/ranger/rifle.conf
+rm ~/.config/ranger/commands.py
+rm ~/.config/ranger/rc.conf
+rm ~/.config/ranger/scope.sh
+
 ln -s $APP_PATH/rifle.conf ~/.config/ranger/rifle.conf
+ln -s $APP_PATH/commands.py ~/.config/ranger/commands.py
+ln -s $APP_PATH/rc.conf ~/.config/ranger/rc.conf
+ln -s $APP_PATH/scope.sh ~/.config/ranger/scope.sh
