9f522600d6bba5a21ffbcc2c5e4206ae7cd7e8e1
klaxalk <Tomas Baca>
2017-07-01 14:24:13 +0200

    added i3gaps submodule

--- appconfig/i3/install.sh
+++ appconfig/i3/install.sh
@@ -12,10 +12,36 @@ while true; do
   if [[ $response =~ ^(y|Y)=$ ]]
   then
 
-    toilet installing i3
-
     # install i3
-    sudo apt-get -y install i3
+    # sudo apt-get -y install i3
+
+    # install dependencies
+
+    sudo expect -c "
+    spawn sudo add-apt-repository ppa:aguignard/ppa
+    expect { 
+      \"ENTER\" {
+        send "\\n"
+        interact
+      }
+    }
+    "
+    sudo apt-get update
+    sudo apt-get -y install libxcb-xrm-dev
+
+    sudo apt-get -y install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+
+    # compile i3
+    cd $APP_PATH/../../submodules/i3/
+    autoreconf --force --install
+    rm -rf build/
+    mkdir -p build && cd build/
+
+    # Disabling sanitizers is important for release versions!
+    # The prefix and sysconfdir are, obviously, dependent on the distribution.
+    ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+    make
+    sudo make install
 
     # for brightness and volume control
     sudo apt-get -y install xbacklight alsa-utils pulseaudio feh arandr acpi
