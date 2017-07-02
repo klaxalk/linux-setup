7b19ec1209e7ec7165353e918cc17810cbd7595d
Tomáš Báča <bacatoma@fel.cvut.cz>
2017-07-02 22:23:18 +0200

    tweaking

--- appconfig/i3/doti3/setColorScheme.sh
+++ appconfig/i3/doti3/setColorScheme.sh
@@ -26,6 +26,7 @@ setColorScheme ~/.i3/i3blocks.conf $COLOR_SCHEME
 setColorScheme ~/.my.vimrc $COLOR_SCHEME
 setColorScheme ~/.Xresources $COLOR_SCHEME
 setColorScheme ~/.tmux.conf $COLOR_SCHEME
+setColorScheme ~/git/linux-setup/appconfig/i3/i3blocks/wifi $COLOR_SCHEME
 
 # reload configuration for urxvt
 xrdb ~/.Xresources
