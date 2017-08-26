837c11e84311a51b4dd6abcdc31b2182bae280a5
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2017-08-26 14:48:15 +0200

    Add missing pacakges when we doing fresh install of the ubontu

--- appconfig/i3/install.sh
+++ appconfig/i3/install.sh
@@ -28,7 +28,10 @@ while true; do
     sudo apt-get update
     sudo apt-get -y install libxcb-xrm-dev
 
-    sudo apt-get -y install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+    sudo apt-get -y install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf
+
+    # install graphical X11 graphical backend with lightdm loading screen
+    sudo apt-get -y install lightdm xserver-xorg
 
     # compile i3
     cd $APP_PATH/../../submodules/i3/
