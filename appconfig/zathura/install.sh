3ab351d7432f1fb20fa99bd896fd0951c435aa52
Tomas Baca <tomas.baca@fel.cvut.cz>
2017-09-02 14:58:12 +0200

    update

--- appconfig/zathura/install.sh
+++ appconfig/zathura/install.sh
@@ -6,15 +6,13 @@ ZATHURA_VERSION=0.3.6
 APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
 
-default=n
+default=y
 while true; do
-  [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mSet up for Latex development? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  [[ -t 0 ]] && { read -t 5 -n 2 -p $'\e[1;32mInstall Zathura? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
   response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
   if [[ $response =~ ^(y|Y)=$ ]]
   then
-
-    sudo apt-get -y install texlive texlive-latex-extra texlive-lang-czechslovak texmaker
 
     sudo apt-get -y install zathura-pdf-poppler libsynctex1 libsynctex-dev libgtk-3-dev xdotool latexmk
 
@@ -24,13 +22,14 @@ while true; do
     # need for zathura compilation
     sudo apt-get -y install libmagic-dev
 
-    # link zathuras config
-    ln -s $APP_PATH/../zathura/zathurarc ~/.config/zathura/zathurarc
-k
     rm -rf /tmp/girara /tmp/zathura
 
     cd /tmp && git clone https://git.pwmt.org/pwmt/girara.git && cd girara && git checkout $GIRARA_VERSION && make && sudo make install
     cd /tmp && git clone https://git.pwmt.org/pwmt/zathura.git && cd zathura && git checkout $ZATHURA_VERSION && make WITH_SYNCTEX=1 && sudo make install
+
+    # link zathuras config
+    mkdir ~/.config/zathura
+    ln -s $APP_PATH/zathurarc ~/.config/zathura/zathurarc
 
     break
   elif [[ $response =~ ^(n|N)=$ ]]
