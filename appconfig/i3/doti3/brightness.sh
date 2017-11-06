d7071b1de7372b7793a3c9c02494b9b0dd445609
Tomas Baca <tomas.baca@fel.cvut.cz>
2017-11-06 09:03:36 +0100

    minor changes

--- appconfig/i3/doti3/brightness.sh
+++ appconfig/i3/doti3/brightness.sh
@@ -1,53 +1,78 @@
 #!/bin/bash
 # author: Tomas Baca
+# ensure running as root
+
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@" 
+fi
 
 INCREMENT=10
 MINIMAL=2
 
+basedir="/sys/class/backlight/"
+
+# get the backlight handler
+handler=$basedir$(ls $basedir)"/"
+
 # find the id of my the touchpad
-CURRENT_BRIGHTNESS=`xbacklight | sed -r "s/^(.*)\..*/\1/g"`
+current_brightness_abs=$(cat $handler"brightness")
 
-notify-send -u low -i mouse "$CURRENT_BRIGHTNESS"
+# get max brightness
+max_brightness=$(cat $handler"max_brightness")
 
+# get current brightness %
+current_brightness_perc=$(( 100 * $current_brightness_abs / $max_brightness ))
 
 if [ $# -eq 0 ]; then
 
-  echo "Current brightness is: $CURRENT_BRIGHTNESS"
+  echo "Current brightness is: $current_brightness_perc"
 
 else
 
   # increase brightness
   if [ "$1" = "+" ]; then
 
-    xbacklight -inc "$INCREMENT"
+    new_brightness_p=$(( $current_brightness_perc + $INCREMENT ))
+    new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
 
     # decrease brightness
   elif [ "$1" = "-" ]; then
 
-    if [[ $(( $CURRENT_BRIGHTNESS - $INCREMENT )) -lt $MINIMAL ]]; then
+    if [[ $(( $current_brightness_perc - $INCREMENT )) -lt $MINIMAL ]]; then
 
-      xbacklight -set "$MINIMAL"
+      new_brightness=$(( $max_brightness * $MINIMAL / 100 ))
 
     else
 
-      xbacklight -dec "$INCREMENT"
+      new_brightness_p=$(( $current_brightness_perc - $INCREMENT ))
+      new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
 
     fi
 
   else
 
-    xbacklight -set "$1"
+    new_brightness=$(( $max_brightness * $1 / 100 ))
 
   fi
 
+  # set the new brightness value
+  chmod 666 $handler"brightness"
+  echo $new_brightness > $handler"brightness"
+  notify-send -u low -i mouse "Brightness $current_brightness_perc%"
+
 fi
 
-CURRENT_BRIGHTNESS=`xbacklight | sed -r "s/^(.*)\..*/\1/g"`
-if [ $CURRENT_BRIGHTNESS -ge 100 ]; then
+# get max brightness
+max_brightness=$(cat $handler"max_brightness")
+
+# get current brightness %
+current_brightness_perc=$(( 100 * $current_brightness_abs / $max_brightness ))
+
+if [ $current_brightness_perc -ge 100 ]; then
 
   notify-send -u low -i mouse "Brightness on MAX"
 
-elif [ $CURRENT_BRIGHTNESS -le $MINIMAL ]; then
+elif [ $current_brightness_perc -le $MINIMAL ]; then
 
   notify-send -u low -i mouse "Brightness on MIN"
 
