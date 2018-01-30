e50f99eb9bc5bf125d96b4c5aebee60604b12c83
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-01-30 14:32:46 +0100

    new macro for compiling sketch

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -21,11 +21,15 @@ while true; do
     ./configure --with-features=huge \
       --enable-multibyte \
       --enable-rubyinterp=yes \
-      --enable-pythoninterp=yes \
-      --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+      --enable-python3interp=yes \
+      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
       --enable-perlinterp=yes \
       --enable-luainterp=yes \
       --enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+      # --enable-pythoninterp=yes \
+      # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+
     cd src
     make
     cd ../
