65c4e371364eed6771e29384aea22a51e436acf6
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-03-28 19:08:02 +0200

    updated brightness script

--- appconfig/i3/doti3/brightness.sh
+++ appconfig/i3/doti3/brightness.sh
@@ -3,7 +3,7 @@
 # ensure running as root
 
 if [ "$(id -u)" != "0" ]; then
-  exec sudo "$0" "$@" 
+  exec sudo "$0" "$@"
 fi
 
 INCREMENT=10
@@ -35,6 +35,10 @@ else
     new_brightness_p=$(( $current_brightness_perc + $INCREMENT ))
     new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
 
+    if [ $new_brightness -ge $max_brightness ]; then
+      new_brightness=$max_brightness
+    fi
+
     # decrease brightness
   elif [ "$1" = "-" ]; then
 
@@ -58,22 +62,25 @@ else
   # set the new brightness value
   chmod 666 $handler"brightness"
   echo $new_brightness > $handler"brightness"
+
+  # get max brightness
+  max_brightness=$(cat $handler"max_brightness")
+
+  # get current brightness %
+  current_brightness_perc=$(( 100 * $current_brightness_abs / $max_brightness ))
+
+  if [ $current_brightness_perc -ge 100 ]; then
+
+    notify-send -u low -t 100 "Brightness on MAX" -h string:x-canonical-private-synchronous:anything -i display-brightness-low-symbolic
+
+  elif [ $current_brightness_perc -le $MINIMAL ]; then
+
+    notify-send -u low -t 100 "Brightness on MIN" -h string:x-canonical-private-synchronous:anything -i display-brightness-low-symbolic
+
+  else
+
   notify-send -u low "Brightness $current_brightness_perc%" -h string:x-canonical-private-synchronous:anything -i display-brightness-low-symbolic
 
-fi
-
-# get max brightness
-max_brightness=$(cat $handler"max_brightness")
-
-# get current brightness %
-current_brightness_perc=$(( 100 * $current_brightness_abs / $max_brightness ))
-
-if [ $current_brightness_perc -ge 100 ]; then
-
-  notify-send -u low -t 100 "Brightness on MAX" -h string:x-canonical-private-synchronous:anything -i display-brightness-low-symbolic
-
-elif [ $current_brightness_perc -le $MINIMAL ]; then
-
-  notify-send -u low -t 100 "Brightness on MIN" -h string:x-canonical-private-synchronous:anything -i display-brightness-low-symbolic
+  fi
 
 fi
--- appconfig/i3/doti3/show_volume.sh
+++ appconfig/i3/doti3/show_volume.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# author: Viktor Walter
-# is supposed to switch between two keyboard layouts
-
-# CURRENT_LAYOUT=$(setxkbmap -query | awk '/layout/{print $2}') 
-
-# echo $CURRENT_LAYOUT | sed 's/.*/\U&/'
 
 OUTPUT=$(~/git/linux-setup/appconfig/i3/i3blocks/volume 5 pulse) 
 
