2cad6eaca58f876ab43ce687d465f04ad2d09b95
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-04-25 18:41:59 +0200

    updated dotprofiler scripts

--- backup_and_deploy.sh
+++ backup_and_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./submodules/dotprofiler/profiler.sh backup ./appconfig/dotprofiler/file_list.txt
-./submodules/dotprofiler/profiler.sh deploy ./appconfig/dotprofiler/file_list.txt
+./backup_configs.sh
+./deploy_configs.sh
