c64050c8bb659893a394a993b7f8f27255524788
klaxalk <Tomas Baca>
2018-04-27 10:24:50 +0200

    moved gpdSetMonitor to scripts

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -182,45 +182,6 @@ setColorScheme() {
   i3-msg restart
 }
 
-gpdSetMonitor() {
-
-  MONITOR=$( echo "STANDALONE\nEXTERNAL" | rofi -dmenu -p "Select monitor:")
-
-  if [[ "$MONITOR" != "STANDALONE" ]] && [[ "$MONITOR" != "EXTERNAL" ]]; then
-    notify-send -u low -t 100 "Wrong choice!" -h string:x-canonical-private-synchronous:anything
-    return 1
-  fi
-
-  notify-send -u low -t 100 "Switching monitor to $MONITOR" -h string:x-canonical-private-synchronous:anything
-
-  xrandr --auto
-
-  case "$SHELL" in 
-    *bash*)
-      RCFILE="$HOME/.bashrc"
-      ;;
-    *zsh*)
-      RCFILE="$HOME/.zshrc"
-      ;;
-  esac
-
-  # change the variable in bashrc
-  /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/dotprofiler/epigen/epigen.vimrc" -E -s -c "%g/.*PROFILER.*MONITOR.*/norm ^/MONITORciwMONITOR_$MONITOR" -c "wqa" -- "$RCFILE"
-
-  source "$RCFILE"
-
-  cd "$GIT_PATH/linux-setup"
-  ./backup_and_deploy.sh
-
-  # reload configuration for urxvt
-  xrdb ~/.Xresources
-
-  # restart i3
-  i3-msg restart
-
-  notify-send -u low -t 100 "Monitor switched" -h string:x-canonical-private-synchronous:anything
-}
-
 # special code for i3 users
 if [ "$USE_I3" = "true" ]; then
 
