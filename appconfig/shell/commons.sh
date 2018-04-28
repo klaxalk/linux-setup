0f0a1709a2a8620b111176a6e5c81c9914bcc53e
Tomas Baca <klaxalk@gmail.com>
2018-04-28 07:30:44 +0200

    moved set_color_scheme to as separate scrirp

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -154,34 +154,6 @@ sourceShellDotfile() {
 }
 alias sb="sourceShellDotfile"
 
-setColorScheme() {
-
-  case "$SHELL" in 
-    *bash*)
-      RCFILE="$HOME/.bashrc"
-      ;;
-    *zsh*)
-      RCFILE="$HOME/.zshrc"
-      ;;
-  esac
-
-  export COLOR_SCHEME="$1"
-
-  # change the variable in bashrc
-  /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/dotprofiler/epigen/epigen.vimrc" -E -s -c "%g/.*PROFILER.*COLORSCHEME.*/norm ^/COLORSCHEMEciwCOLORSCHEME_$COLOR_SCHEME" -c "wqa" -- "$RCFILE"
-
-  source "$RCFILE"
-
-  cd "$GIT_PATH/linux-setup"
-  ./backup_and_deploy.sh
-
-  # reload configuration for urxvt
-  xrdb ~/.Xresources
-
-  # restart i3
-  i3-msg restart
-}
-
 # special code for i3 users
 if [ "$USE_I3" = "true" ]; then
 
