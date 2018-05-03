4b40e91849d395c3b0d5bd5087de9c599570e2fa
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-05-03 09:42:48 +0200

    update git alias

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -117,6 +117,11 @@ git() {
 
         command git "$@"
 
+        case $* in pull*)
+          echo "Updating git submodules"
+          command git submodule update --init --recursive
+        esac
+
         if [[ "$?" == "0" ]]; then
 
           bash -c "$PROFILER deploy $GIT_PATH/linux-setup/appconfig/dotprofiler/file_list.txt"
@@ -125,10 +130,18 @@ git() {
 
       else
         command git "$@"
+        case $* in pull*)
+          echo "Updating git submodules"
+          command git submodule update --init --recursive
+        esac
       fi
 
     else
       command git "$@"
+      case $* in pull*)
+        echo "Updating git submodules"
+        command git submodule update --init --recursive
+      esac
     fi
 
     ;;
