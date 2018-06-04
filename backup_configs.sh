d04f417ce5b2c0fc6b4ee0e013def2cc99991df8
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-06-04 17:39:17 +0200

    updated dotprofiler

--- backup_configs.sh
+++ backup_configs.sh
@@ -1,3 +1,6 @@
+# - make the script run in bash/zsh while having its dotfile sourced
+# - this is important when there are variables exported, which might
+#   be used by this script
 PNAME=$( ps -p "$$" -o comm= )
 SNAME=$( echo "$SHELL" | grep -Eo '[^/]+/?$' )
 if [ "$PNAME" != "$SNAME" ]; then
