9531c69c74e0ffd3616c26c4ae2bd8d8e9b90e00
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-06-30 19:26:27 +0200

    fixed toggle_touchpad for default i3 binding

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if synclient -l | grep "TouchpadOff .*=.*0" ; then
-    synclient TouchpadOff=1 ;
+  synclient TouchpadOff=1 ;
+
+  # banish mouse
+  xdotool mousemove 10000 10000
 else
-    synclient TouchpadOff=0 ;
+  synclient TouchpadOff=0 ;
 fi
