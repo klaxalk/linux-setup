579b7c3eadcbb7fe373a7e24a877b666ebcc5c91
Tomas Baca <klaxalk@gmail.com>
2019-01-29 18:01:00 +0100

    added the best and the most ultimate git forking script!

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -43,10 +43,10 @@ killp() {
 
   for child in $(ps -o pid,ppid -ax | \
     awk "{ if ( \$2 == $pes ) { print \$1 }}")
-do
-  # echo "Killing child process $child because ppid = $pes"
-  killp $child
-done
+    do
+      # echo "Killing child process $child because ppid = $pes"
+      killp $child
+    done
 
 # echo "killing $1"
 kill -9 "$1" > /dev/null 2> /dev/null
@@ -146,38 +146,38 @@ git() {
           command git submodule sync
           echo "Updating git submodules"
           command git submodule update --init --recursive
-        esac
+      esac
 
-        if [[ "$?" == "0" ]]; then
-          bash -c "$PROFILE_MANAGER deploy $GIT_PATH/linux-setup/appconfig/profile_manager/file_list.txt"
-        fi
-
-      else
-        command git "$@"
-        case $* in pull*|checkout*)
-          echo "Syncing git submodules"
-          command git submodule sync
-          echo "Updating git submodules"
-          command git submodule update --init --recursive
-        esac
+      if [[ "$?" == "0" ]]; then
+        bash -c "$PROFILE_MANAGER deploy $GIT_PATH/linux-setup/appconfig/profile_manager/file_list.txt"
       fi
 
-      else
-        command git "$@"
-        case $* in pull*|checkout*)
-          echo "Syncing git submodules"
-          command git submodule sync
-          echo "Updating git submodules"
-          command git submodule update --init --recursive
-        esac
-      fi
+    else
+      command git "$@"
+      case $* in pull*|checkout*)
+        echo "Syncing git submodules"
+        command git submodule sync
+        echo "Updating git submodules"
+        command git submodule update --init --recursive
+    esac
+  fi
 
-    ;;
-  *)
-    command git "$@"
-    ;;
+else
+  command git "$@"
+  case $* in pull*|checkout*)
+    echo "Syncing git submodules"
+    command git submodule sync
+    echo "Updating git submodules"
+    command git submodule update --init --recursive
+esac
+fi
 
-  esac
+;;
+*)
+  command git "$@"
+  ;;
+
+esac
 }
 
 getRcFile() {
@@ -385,21 +385,105 @@ slack() {
   if [ -z $1 ]
   then
     SLACK_NAME=$(echo "mrs
-eagle" | rofi -i -dmenu -no-custom -p "Select slack")
+    eagle" | rofi -i -dmenu -no-custom -p "Select slack")
   else
     SLACK_NAME=${1}
   fi
 
   case ${SLACK_NAME} in
     mrs)
-    SLACK_CFG=~/git/notes/slack/mrsworkspace
-    ;;
+      SLACK_CFG=~/git/notes/slack/mrsworkspace
+      ;;
     eagle)
-    SLACK_CFG=~/git/notes/slack/drone-eagleone
-    ;;
+      SLACK_CFG=~/git/notes/slack/drone-eagleone
+      ;;
   esac
 
   $SLACK_BIN -config $SLACK_CFG
+}
+
+repo_to_local() {
+
+  MY_PATH=`dirname "$0"`
+  MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+  USERNAME="klaxalk"
+  ADDRESS="localhost"
+  SUBFOLDER="test"
+
+  # parse the .gitmodules files in the PATH
+  if [ -f "$MY_PATH/$1/.gitmodules" ]; then
+
+    # find each module in the .gitmodules file and extract its relative path from the doublequotes
+    SUBMODULES=($( cat "$MY_PATH/$1/.gitmodules" | grep "^\[submodule" | cut -d "\"" -f2 | cut -d "\"" -f1 ))
+
+    cp $MY_PATH/$1/.gitmodules $MY_PATH/$1/.gitmodules_new
+
+    # for each submodule
+    for submodule in $SUBMODULES; do
+
+      # recursively find its submodules
+      if [ -z "$1" ]; then # if we are in the root repo
+        repo_to_local "$submodule"
+      else
+        repo_to_local "$1/$submodule"
+      fi
+
+      # extract the name in the superrepo
+      REPO_NAME=$( echo $submodule | sed -r 's/.*\/(.+)/\1/g')
+      echo REPO_NAME: $REPO_NAME
+
+      # extract the submodule server path
+      CMD="cat '$MY_PATH/$1/.gitmodules' | grep -e 'url.*$REPO_NAME' | sed -r 's/.*:(.*)$REPO_NAME/\1/g'"
+      SUB_PATH=$( eval $CMD )
+      echo SUB_PATH: $SUB_PATH
+
+      # create the bare repo
+      CMD="ssh $USERNAME@$ADDRESS 'mkdir -p ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; cd ~/$SUBFOLDER/$SUB_PATH$REPO_NAME; git init --bare'"
+      eval $CMD
+
+      # push the local repo
+      cd "$MY_PATH/$1/$submodule"
+      git remote remove local
+      git remote add local "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
+      git push --all local -u
+      cd "$MY_PATH"
+      git config --file=.gitmodules_new "submodule.$submodule.url" "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
+      git submodule sync > /dev/null
+
+    done
+
+    cp $MY_PATH/$1/.gitmodules_new $MY_PATH/$1/.gitmodules
+
+  fi
+
+  # fix the super repo
+  if [ -z "$1" ]; then
+
+    echo ""
+
+    # extract the name in the superrepo
+    CMD="git remote -v | grep origin | head -n 1 | cut -d ":" -f2 | sed -r 's/.*\/(.+)\s.*$/\1/g'"
+    REPO_NAME=$( eval $CMD )
+    echo REPO_NAME: $REPO_NAME
+
+    CMD="git remote -v | grep origin | head -n 1 | cut -d ":" -f2 | sed -r 's/(.*)$REPO_NAME.*/\1/g'"
+    SUB_PATH=$( eval $CMD )
+    echo SUB_PATH: $SUB_PATH
+
+    # create the bare repo
+    CMD="ssh $USERNAME@$ADDRESS 'mkdir -p ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; cd ~/$SUBFOLDER/$SUB_PATH$REPO_NAME; git init --bare'"
+    eval $CMD
+
+    # push the local repo
+    git remote remove local
+    git remote add local "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
+    git push --all local -u
+    git add .gitmodules
+    git commit -m "switched .gitmodules to local"
+    git push
+
+  fi
 }
 
 CURRENT_PATH=`pwd`
