eca07975159c2748c0d9a6f9c77d734ede0d05a5
Tomas Baca <klaxalk@gmail.com>
2019-01-30 20:22:02 +0100

    working on repo forker

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -430,17 +430,36 @@ repo_to_local() {
       fi
 
       # extract the name in the superrepo
-      REPO_NAME=$( echo $submodule | sed -r 's/.*\/(.+)/\1/g')
+      echo SUBMODULE: $submodule
+      REPO_NAME=$( echo $submodule | sed -r 's/.*\/([^\/]+)/\1/g' )
       echo REPO_NAME: $REPO_NAME
 
       # extract the submodule server path
-      CMD="cat '$MY_PATH/$1/.gitmodules' | grep -e 'url.*$REPO_NAME' | sed -r 's/.*:(.*)$REPO_NAME/\1/g'"
+      CMD="cat '$MY_PATH/$1/.gitmodules' | grep -e 'url.*$REPO_NAME' | sed -r 's/.*:(.*)$REPO_NAME.*/\1/g'"
       SUB_PATH=$( eval $CMD )
       echo SUB_PATH: $SUB_PATH
 
-      # create the bare repo
-      CMD="ssh $USERNAME@$ADDRESS 'mkdir -p ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; cd ~/$SUBFOLDER/$SUB_PATH$REPO_NAME; git init --bare'"
+      # check if the repo was actually created
+      CMD="ssh $USERNAME@$ADDRESS 'test -d ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME'"
       eval $CMD
+      RET=$?
+      if [[ "$RET" == "0" ]]; then
+        echo "Cannot create the repo, already exists...\n"
+        continue
+      fi
+
+      # create the bare repo
+      CMD="ssh $USERNAME@$ADDRESS 'mkdir -p ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; cd ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; git init --bare'"
+      eval $CMD
+
+      # check if the repo was actually created
+      CMD="ssh $USERNAME@$ADDRESS 'test -d ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME'"
+      eval $CMD
+      RET=$?
+      if [[ "$RET" != "0" ]]; then
+        echo "Cannot create the repo, quitting...\n"
+        continue
+      fi
 
       # push the local repo
       cd "$MY_PATH/$1/$submodule"
@@ -448,19 +467,17 @@ repo_to_local() {
       git remote add local "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
       git push --all local -u
       cd "$MY_PATH"
-      git config --file=.gitmodules_new "submodule.$submodule.url" "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
+      git config --file=.gitmodules_new "submodule.$submodule.url" "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH/$REPO_NAME"
       git submodule sync > /dev/null
 
     done
 
-    cp $MY_PATH/$1/.gitmodules_new $MY_PATH/$1/.gitmodules
+    cp "$MY_PATH/$1/.gitmodules_new" "$MY_PATH/$1/.gitmodules"
 
   fi
 
   # fix the super repo
   if [ -z "$1" ]; then
-
-    echo ""
 
     # extract the name in the superrepo
     CMD="git remote -v | grep origin | head -n 1 | cut -d ":" -f2 | sed -r 's/.*\/(.+)\s.*$/\1/g'"
@@ -471,13 +488,32 @@ repo_to_local() {
     SUB_PATH=$( eval $CMD )
     echo SUB_PATH: $SUB_PATH
 
+    # check if the repo was actually created
+    CMD="ssh $USERNAME@$ADDRESS 'test -d ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME'"
+    eval $CMD
+    RET=$?
+    if [[ "$RET" == "0" ]]; then
+      echo "Cannot create the repo, already exists..."
+      return
+    fi
+
     # create the bare repo
     CMD="ssh $USERNAME@$ADDRESS 'mkdir -p ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME; cd ~/$SUBFOLDER/$SUB_PATH$REPO_NAME; git init --bare'"
+    eval "$CMD"
+
+    # check if the repo was actually created
+    CMD="ssh $USERNAME@$ADDRESS 'test -d ~/$SUBFOLDER/$SUB_PATH/$REPO_NAME'"
     eval $CMD
+    RET=$?
+
+    if [[ "$RET" != "0" ]]; then
+      echo "Cannot create the repo, quitting..."
+      return
+    fi
 
     # push the local repo
     git remote remove local
-    git remote add local "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH$REPO_NAME"
+    git remote add local "$USERNAME@$ADDRESS:~/$SUBFOLDER/$SUB_PATH/$REPO_NAME"
     git push --all local -u
     git add .gitmodules
     git commit -m "switched .gitmodules to local"
