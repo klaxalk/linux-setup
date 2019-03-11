30e597ed934cee0b3109926c275bb676c8f1e2b2
Tomas Baca <klaxalk@gmail.com>
2019-03-11 15:33:04 +0100

    updated catkin init alias to contain cxx flags

--- appconfig/shell/commons.sh
+++ appconfig/shell/commons.sh
@@ -366,10 +366,11 @@ catkin() {
     ROOT_DIR=`git rev-parse --show-toplevel` 2> /dev/null
 
     command catkin "$@"
-    command catkin config --profile default --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    command catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    command catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    command catkin profile set default
+    command catkin config --profile debug --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    command catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -O3 -fopenmp -march=native -fno-diagnostics-color'  -DCMAKE_C_FLAGS='-O3 -fopenmp -march=native -fno-diagnostics-color'
+    command catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -O3 -fopenmp -march=native -fno-diagnostics-color' -DCMAKE_C_FLAGS='-O3 -fopenmp -march=native -fno-diagnostics-color'
+
+    command catkin profile set reldeb
     ;;
   *)
     command catkin "$@"
