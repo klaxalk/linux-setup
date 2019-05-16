8493bb58dc69d1eb226b9d20a96b21892b6da859
dan <hertdani@fel.cvut.cz>
2019-05-16 13:46:44 +0200

    more indie

--- appconfig/i3/doti3/indie_redux.py
+++ appconfig/i3/doti3/indie_redux.py
@@ -25,10 +25,21 @@ elif day == 4:
     response = response[response.find("FRIDAY"):-1]
 else:
     response = "No Indian on the weekends!!!!"
-response = response.replace('1.', '\n1.')
-response = response.replace('2.', '\n2.')
+
+response = response.replace('1.', '\n1.', 1)
+response = response.replace('2.', '\n2.', 1)
 response = response.replace('3.', '\n3.', 1)
 response = response.replace('4.', '\n4.', 1)
 response = response.replace('5.', '\n5.', 1)
 response = response.replace('6.', '\n6.')
-print(response)
+
+chic = response[response.find("3."):-1]
+chic = chic.partition("\n")[0]
+chic = chic[chic.find("/"):chic.find("  ")]
+chic = chic[1:]
+
+pork = response[response.find("4."):-1]
+pork = pork.partition("\n")[0]
+pork = pork[pork.find("/"):pork.find("  ")]
+
+print(chic+pork)
