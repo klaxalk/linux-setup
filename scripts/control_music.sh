e9c0a2177761915a4d0174f4d06b423459e7bd46
Matouš Vrba <vrbamato@MyslPlacka>
2019-06-05 13:56:53 +0200

    added install script for playerctl to install from debian repos (it's unavailable in ubuntu 18.04)

--- scripts/control_music.sh
+++ scripts/control_music.sh
@@ -1,19 +1,51 @@
 #/usr/bin/env bas
 
-### Firefox toggle playback using the Play/Pause addon (https://addons.mozilla.org/en-US/firefox/addon/play-pause/)
-xdotool search --name "Mozilla Firefox" key Alt_L+Shift_L+p
+action="toggle"
+if [ $# -ge 1 ]; then
+  action=$1
+fi
 
-### Spotify toggle playback
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+playerctl -v > /dev/null 2>&1
+pctl_res=$?
+if [ $pctl_res -eq 0 ]; then
+  cmd_toggle="playerctl play-pause";
+  cmd_play="playerctl play";
+  cmd_pause="playerctl pause";
+  cmd_previous="playerctl previous";
+  cmd_next="playerctl next";
+else
+  cmd_toggle="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause";
+  cmd_play="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play";
+  cmd_pause="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause";
+  cmd_previous="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous";
+  cmd_next="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next";
+fi
 
-### Spotify play
-## dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
+case $action in
+     "toggle")      
+          ### Firefox toggle playback using the Play/Pause addon (https://addons.mozilla.org/en-US/firefox/addon/play-pause/)
+          # xdotool search --name "Mozilla Firefox" key Alt_L+Shift_L+p
+          ### Spotify toggle playback
+          ($cmd_toggle) > /dev/null
+          ;;
+     "play")      
+          ### Spotify play
+          ($cmd_play) > /dev/null
+          ;;
+     "pause")
+          ### Spotify pause
+          ($cmd_pause) > /dev/null
+          ;; 
+     "previous")
+          ### Spotify prev song
+          ($cmd_previous) > /dev/null
+          ;;
+     "next")
+          ### Spotify next song
+          ($cmd_next) > /dev/null
+          ;;
+     *)
+          echo "Invalid option! Allowed options are: toggle|play|pause|previous|next or none (equals to toggle)." > /dev/stderr
+          ;;
+esac
 
-### Spotify pause
-## dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-
-### Spotify prev song
-## dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
-
-### Spotify next song
-## dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
