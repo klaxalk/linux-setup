e103e4d78e8db44779eb76312f9824ad7f3fdb79
Pavel Petráček <petrapa6@fel.cvut.cz>
2019-07-16 09:36:39 +0200

    Change pavel's lab monitor

--- miscellaneous/arandr_scripts/pavel/hp_lab.sh
+++ miscellaneous/arandr_scripts/pavel/hp_lab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --rotate normal --pos 0x0 --primary --output eDP-1-1 --mode 1920x1080 --rotate normal --pos 1920x550
+xrandr --output eDP-1-1 --mode 1920x1080 --rotate normal --pos 0x850 --output HDMI-0 --mode 1920x1080 --rotate normal --pos 1920x0 --primary
