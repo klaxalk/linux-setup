d1ece88cfaa773934d5c4f42563d8a5fd3e0134c
Matouš Vrba <vrbamato@MyslPlacka>
2019-07-25 18:25:16 +0200

    added reconfigurator script to rofi

--- appconfig/i3/make_launchers.sh
+++ appconfig/i3/make_launchers.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/zsh
 
-script_path=$1
+script_path=../../scripts
+if [ $# -gt 2 ]; then
+  script_path=$1;
+fi
 
 $script_path/makeLauncher.sh "rviz" "RViz"
 $script_path/makeLauncher.sh "rosrun plotjuggler PlotJuggler" "PlotJuggler"
 $script_path/makeLauncher.sh "rqt_image_view" "RQt Image Viewer"
+$script_path/makeLauncher.sh "rosrun rqt_reconfigure rqt_reconfigure" "RQt Dynamic Reconfigurator"
 $script_path/makeLauncher.sh "rosrun rqt_tf_tree rqt_tf_tree" "RQt TF Tree Viewer"
 $script_path/makeLauncher.sh "rqt" "RQt Menu"
