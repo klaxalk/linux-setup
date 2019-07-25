6c940c6ea87d2fc1b74fc7127d9b089ebf3f3a48
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-07-25 18:24:39 +0200

    polished make_launchers.sh

--- appconfig/i3/make_launchers.sh
+++ appconfig/i3/make_launchers.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
-script_path=$1
+# get the path to this script
+APP_PATH=`dirname "$0"`
+APP_PATH=`( cd "$APP_PATH" && pwd )`
 
-$script_path/makeLauncher.sh "rviz" "RViz"
-$script_path/makeLauncher.sh "rosrun plotjuggler PlotJuggler" "PlotJuggler"
-$script_path/makeLauncher.sh "rqt_image_view" "RQt Image Viewer"
-$script_path/makeLauncher.sh "rosrun rqt_tf_tree rqt_tf_tree" "RQt TF Tree Viewer"
-$script_path/makeLauncher.sh "rqt" "RQt Menu"
+cd $APP_PATH/../../scripts/
+
+./makeLauncher.sh "rviz" "RViz"
+./makeLauncher.sh "rosrun plotjuggler PlotJuggler" "PlotJuggler"
+./makeLauncher.sh "rqt_image_view" "RQt Image Viewer"
+./makeLauncher.sh "rosrun rqt_tf_tree rqt_tf_tree" "RQt TF Tree Viewer"
+./makeLauncher.sh "rqt" "RQt Menu"
