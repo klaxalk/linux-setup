2216436d332c5db81879ebffba9e5d5752fbfe9e
Tomas Baca <klaxalk@gmail.com>
2020-03-31 00:10:18 +0200

    added cpp17 to default ycm flags

--- appconfig/vim/dotycm_extra_conf.py
+++ appconfig/vim/dotycm_extra_conf.py
@@ -122,6 +122,7 @@ default_flags = [
     '-DNDEBUG',
     '-DUSE_CLANG_COMPLETER',
     '-Qunused-arguments',
+    '-std=c++17',
     # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know
     # which language to use when compiling headers. So it will guess. Badly. So
     # C++ headers will be compiled as C headers. You don't want that so ALWAYS
