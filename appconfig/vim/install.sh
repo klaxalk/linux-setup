ac4247c11dff9b40d4d7a62cda49ddf96921ae3a
Tomas Baca <tomas.baca@fel.cvut.cz>
2020-05-06 16:22:31 +0200

    added ruby dependency for vim

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -46,7 +46,7 @@ while true; do
       sudo apt -y install libgnome2-dev libgnomeui-dev libbonoboui2-dev
     fi
 
-    sudo apt -y install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev clang-format
+    sudo apt -y install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev clang-format ruby-dev
 
     sudo -H pip3 install rospkg
 
