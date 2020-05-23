fec442a86d77997ec5a003ba1c17b865ae38555e
Petr Stibinger <stibipet@fel.cvut.cz>
2020-05-23 18:23:31 +0200

    petr: add touchpad toggle

---
+++ miscellaneous/arandr_scripts/petr/dell_dock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-1 --off --output DP-3 --off --output DP-1 --off --output HDMI-2 --off
