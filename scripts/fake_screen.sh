5ad0d4604ed932ddbfa1e4f8e982781704facdc6
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2020-05-25 17:45:23 +0200

    added comments and usage example into fake_screen.sh

--- scripts/fake_screen.sh
+++ scripts/fake_screen.sh
@@ -10,4 +10,13 @@ if [ $# -ge 1 ]; then
 else
   screen=":1"
 fi
+echo "HELP: This script sets up a virtual X server to enable simulation on a computer without a display 
+      Usage:
+      - run this script in a terminal, then launch the simulation with
+           export DISPLAY=:1
+        or choose a different display (can be supplied to this script as a parameter)
+"
+echo "Running the script:"
+echo "- Creating FAKE display -> DISPLAY=${screen}"
+echo "- press \"ctrl+c\" to kill the script"
 Xvfb "$screen" -screen 0 1280x1024x24
