ed6a11710926c08c04ab55786cc2a596b6e587f7
Tomas Baca <klaxalk@gmail.com>
2020-06-21 17:44:23 +0200

    added elgato

---
+++ appconfig/elgato_streamdeck/install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to this script
+APP_PATH=`dirname "$0"`
+APP_PATH=`( cd "$APP_PATH" && pwd )`
+
+unattended=0
+subinstall_params=""
+for param in "$@"
+do
+  echo $param
+  if [ $param="--unattended" ]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
+
+default=n
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=$default
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mInstall Elgato Streamdeck support? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
+
+    sudo apt -y install libhidapi-hidraw0 libudev-dev libusb-1.0-0-dev
+
+    sudo usermod -a -G plugdev `whoami`
+
+    sudo cp ./99-streamdeck.rules /etc/udev/rules.d
+
+    sudo udevadm control --reload-rules
+
+    pip3 install --user streamdeck_ui
+
+    # symlink the settings
+    rm -rf $HOME/.streamdeck_icons
+    ln -sf $APP_PATH/streamdeck_icons $HOME/.streamdeck_icons
+
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+done
