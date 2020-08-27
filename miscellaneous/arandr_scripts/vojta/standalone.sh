e747fb9f4365bac17f1c3ac2d5a1a89a0ef50d16
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2020-08-27 10:30:33 +0200

    updated arrandr scripts

--- miscellaneous/arandr_scripts/vojta/standalone.sh
+++ miscellaneous/arandr_scripts/vojta/standalone.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-2 --off --output HDMI-1-1 --off
+xrandr --output HDMI-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off
