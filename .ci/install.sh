00b22f4d670d211610b361c4a9a3915965e453b2
Matouš Vrba <vrbamato@fel.cvut.cz>
2020-12-23 21:51:33 +0100

    removed installation of redundant and problematic package gce-compute-image-packages

--- .ci/install.sh
+++ .ci/install.sh
@@ -14,10 +14,6 @@ if [ "$distro" = "18.04" ]; then
   sudo apt-mark hold postgresql-10
 fi
 
-# the "gce-compute-image-packages" package often freezes the installation
-# the installation freezes when it tries to manage some systemd services
-((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120 apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
-
 sudo apt -y upgrade --fix-missing
 
 sudo apt-get install dpkg git
