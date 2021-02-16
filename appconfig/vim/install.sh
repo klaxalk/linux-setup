f65d4650c9be435bdf05c42aec07ee9fd4a28c61
Tomas Baca <klaxalk@gmail.com>
2021-02-16 06:30:27 +0100

    removed vim gui build

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -58,15 +58,7 @@ while true; do
       --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
       --enable-perlinterp=yes \
       --enable-luainterp=yes \
-      --enable-gui=gtk2 --enable-cscope --prefix=/usr
-
-      ## add for python2
-      # --enable-pythoninterp=yes \
-      # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-
-      ## add for python3
-      # --enable-python3interp=yes \
-      # --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+      --enable-cscope --prefix=/usr
 
       cd src
       make
