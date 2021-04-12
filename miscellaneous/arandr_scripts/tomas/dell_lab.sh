582233b05f7fa000060103232fbc53c957b7f9a2
Tomas Baca <klaxalk@gmail.com>
2021-04-12 06:51:13 +0400

    updated tomas monitor

--- miscellaneous/arandr_scripts/tomas/dell_lab.sh
+++ miscellaneous/arandr_scripts/tomas/dell_lab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
