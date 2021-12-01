0bb6dd4bf8187dcabd492461c9cebd1de0fd640d
Tomas Baca <klaxalk@gmail.com>
2021-12-01 12:27:41 +0100

    updated snippets

--- miscellaneous/arandr_scripts/tomas/desktop_normal.sh
+++ miscellaneous/arandr_scripts/tomas/desktop_normal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x310 --rotate normal --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-4 --off
