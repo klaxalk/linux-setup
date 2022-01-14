621df5238524f0189a9f0d6e3fde85cb16f14a3e
Pavel Petracek <petrapa6@fel.cvut.cz>
2022-01-14 15:45:17 +0100

    update papis_add.sh

--- scripts/papis_add.sh
+++ scripts/papis_add.sh
@@ -71,26 +71,109 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check if first argument is bib file (is not parameter and ends with .bib)
-if [[ "$BIB" == -* ]] || [[ "$BIB" != *.bib ]]; then
-  echo "First argument is mandatory to be the .bib file path."
+if [[ "$BIB" == -* ]] || [[ "$BIB" != *.bib ]] || [ ! -f "$BIB" ]; then
+  echo "First argument is mandatory to be an existing .bib file path."
   printHelp
   exit -2
 fi
 
 ## #}
 
+COLOR_HIGHLIGHT='\033[0;32m'
+STYLE_NONE='\033[0m'
+TEXT_BOLD='\033[1m'
 echo "###################"
-echo bib:      $BIB
-echo ref:      $REF
-echo pdf:      $PDF
-echo addendum: $ADDENDUM
-echo keywords: $KEYWORDS
+echo -e bib:      ${COLOR_HIGHLIGHT}$BIB${STYLE_NONE}
+echo -e ref:      ${COLOR_HIGHLIGHT}${TEXT_BOLD}$REF${STYLE_NONE}
+echo -e pdf:      ${COLOR_HIGHLIGHT}$PDF${STYLE_NONE}
+echo -e addendum: ${COLOR_HIGHLIGHT}$ADDENDUM${STYLE_NONE}
+echo -e keywords: ${COLOR_HIGHLIGHT}$KEYWORDS${STYLE_NONE}
 echo "###################"
+
+## #{ Preprocess input bib file
+
+# Get vim
+if [ -x "$(whereis nvim | awk '{print $2}')" ]; then
+  VIM_BIN="$(whereis nvim | awk '{print $2}')"
+  HEADLESS="--headless"
+elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
+  VIM_BIN="$(whereis vim | awk '{print $2}')"
+  HEADLESS=""
+fi
+
+# Copy bib file
+cp $BIB /tmp/tmp.bib
+BIB=/tmp/tmp.bib
+
+# Replace czech characters
+$VIM_BIN $HEADLESS -nEs -c '%g/author.*=/s/{//g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/author.*=/s/}//g' -c "wqa" -- "$BIB"
+
+$VIM_BIN $HEADLESS -nEs -c '%s/ě/e/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/š/s/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/č/c/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/ř/r/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/ž/z/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/ý/y/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/á/a/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/í/i/g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%s/é/e/g' -c "wqa" -- "$BIB"
+
+$VIM_BIN $HEADLESS -nEs -c '%g/author.*=/s/\\v//g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c "%g/author.*=/s/\\\'//g" -c "wqa" -- "$BIB"
+
+$VIM_BIN $HEADLESS -nEs -c '%g/author.*=/norm f=wi{$a F,i}' -c "wqa" -- "$BIB"
+
+# Remove double {{ }} from title
+$VIM_BIN $HEADLESS -nEs -c '%g/title.*=/s/{//g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/title.*=/s/}//g' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/title.*=/norm f=wi{$bi}' -c "wqa" -- "$BIB"
+
+# Month field correction
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=.*/s/}//g' -c "wqa" -- "$BIB" # delete } brackets around the month
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=.*/s/{//g' -c "wqa" -- "$BIB" # delete { brackets around the month
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/norm Vu' -c "wqa" -- "$BIB" # lowercase
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/january/1' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/february/2' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/march/3' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/april/4' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/may/5' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/june/6' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/july/7' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/august/8' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/september/9' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/october/10' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/november/11' -c "wqa" -- "$BIB"
+$VIM_BIN $HEADLESS -nEs -c '%g/month.*=/s/december/12' -c "wqa" -- "$BIB"
+
+# Ensure there is -- between pages
+$VIM_BIN $HEADLESS -nEs -c '%g/pages.*=/s/\d\zs-\ze\d/--' -c "wqa" -- "$BIB"
+
+## #}
+
+# Get papis database directory
+PAPIS_DIR=`papis -l papers config dirs | cut -d "\"" -f2` # find dirs specified in papis config, e.g., ["~/git/bibligraphy", "~/Dropbox/papers"] # EPIGEN...
+if [[ "$PAPIS_DIR" = ~\/* ]]; then PAPIS_DIR="${PAPIS_DIR/#\~/$HOME}"; fi # expand tilde
+
+if [ ! -d "$PAPIS_DIR" ]; then
+  echo -e "Could not find path to papis ${COLOR_HIGHLIGHT}papers${STYLE_NONE} database:"
+  echo -e "   ${COLOR_HIGHLIGHT}papis -l papers config dirs${STYLE_NONE}: `papis -l papers config dirs`"
+  echo -e "   ${COLOR_HIGHLIGHT}papis -l papers config dirs | cut -d \" -f2${STYLE_NONE}:" `papis -l papers config dirs | cut -d "\"" -f2`
+  echo -e "   ${COLOR_HIGHLIGHT}\$PAPIS_DIR${STYLE_NONE}: $PAPIS_DIR"
+  exit -3
+fi
 
 # Add bibtex to papis
 if [ -z "$REF" ]; then
   papis add --batch --from bibtex "$BIB"
 else
+
+  # Check if ref does not exist
+  if [ -d "$PAPIS_DIR/$REF" ]; then
+    echo -e "Reference ${COLOR_HIGHLIGHT}${TEXT_BOLD}$REF${STYLE_NONE} already exists in the database."
+    exit 0
+  fi
+
   # Add to papis under REF keyword
   papis add --batch --from bibtex "$BIB" --folder-name "$REF"
 
@@ -99,9 +182,24 @@ else
 
   # Add pdf file
   if [ ! -z "$PDF" ]; then papis addto --doc-folder "$REF" --copy-pdf -f "$PDF"; fi
-
 fi
 
 # Update item parameters
 if [ ! -z "$ADDENDUM" ]; then papis update --doc-folder "$REF" -s addendum "$ADDENDUM"; fi
 if [ ! -z "$KEYWORDS" ]; then papis update --doc-folder "$REF" -s keywords "$KEYWORDS"; fi
+
+## #{ Postprocess output yaml file
+
+if [ ! -z "$REF" ] && [ -d "$PAPIS_DIR/$REF" ]; then
+
+    FILE=$PAPIS_DIR/$REF/info.yaml
+    echo -e "Processing fields in file: $PAPIS_DIR/${COLOR_HIGHLIGHT}$REF${STYLE_NONE}/info.yaml"
+
+    # Move ref and addendum up
+    $VIM_BIN $HEADLESS -nEs -c '%g/addendum:/norm ddggP' -c "wqa" -- "$FILE"
+    $VIM_BIN $HEADLESS -nEs -c '%g/ref:/norm ddggP' -c "wqa" -- "$FILE"
+
+fi
+
+## #}
+
