9b3341b48ab2aa411b6eace0bfbf045a06f7c1d1
daniel <smrckda1@fel.cvut.cz>
2022-03-25 09:53:54 +0100

    fixed typo in daniel's monitor script

--- scripts/daniel_set_monitor.sh
+++ scripts/daniel_set_monitor.sh
@@ -15,7 +15,7 @@ HOME
 PRESENTATION
 STANDALONE" | rofi -i -dmenu -p "Select setup:")
 
-if [[ "$MONITOR" != "LAB" ]] && [[ "$MONITOR" != "LAB" ]] && [[ "$MONITOR" != "HOME" ]] && [[ "$MONITOR" != "PRESENTATION" ]] && [[ "$MONITOR" != "STANDALONE" ]]; then
+if [[ "$MONITOR" != "GPU" ]] && [[ "$MONITOR" != "LAB" ]] && [[ "$MONITOR" != "HOME" ]] && [[ "$MONITOR" != "PRESENTATION" ]] && [[ "$MONITOR" != "STANDALONE" ]]; then
   notify-send -u low -t 100 "Wrong choice!" -h string:x-canonical-private-synchronous:anything
   exit
 fi
