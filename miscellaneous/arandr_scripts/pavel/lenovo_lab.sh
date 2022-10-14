8ab3f2d352c692ecbc36435e70a881f8a1945240
Pavel Petracek <petrapa6@fel.cvut.cz>
2022-10-14 16:55:31 +0200

    update pivel's arandr scripts

--- miscellaneous/arandr_scripts/pavel/lenovo_lab.sh
+++ miscellaneous/arandr_scripts/pavel/lenovo_lab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1080 --pos 0x996 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --mode 1920x1200 --pos 3840x273 --rotate normal --output DisplayPort-4 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP --mode 1920x1080 --pos 0x880 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1200 --pos 3840x418 --rotate normal --output DisplayPort-3 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-4 --off
