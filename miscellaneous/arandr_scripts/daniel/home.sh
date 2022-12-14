06373d3b40b91c2064a06927ab08800777f8fbce
Daniel Smrcka <smrckda1@fel.cvut.cz>
2022-12-14 14:08:07 +0100

    updated daniel's font and arandr scripts

--- miscellaneous/arandr_scripts/daniel/home.sh
+++ miscellaneous/arandr_scripts/daniel/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
--- miscellaneous/arandr_scripts/daniel/standalone.sh
+++ miscellaneous/arandr_scripts/daniel/standalone.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --off --output DP-1 --off --output LVDS-1 --mode 1366x768 --pos 0x0 --primary --rotate normal --output HDMI-2 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off
