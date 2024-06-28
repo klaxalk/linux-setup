d8822434288ed3a71f06422a614135e7f9de17f1
Matej Petrlik <matej.petrlik@fel.cvut.cz>
2024-06-28 11:23:42 +0200

    added matej's arandr scripts

---
+++ miscellaneous/arandr_scripts/matej/lab_asus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --mode 1920x1200 --pos 1920x0 --rotate normal
