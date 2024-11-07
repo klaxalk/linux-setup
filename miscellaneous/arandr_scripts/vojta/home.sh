7de89b6524341e8cac18bb1db0ff11b4148888b2
Vojtech Spurny <spurny@fly4future.com>
2024-11-07 20:34:42 +0100

    Vojta add new monitor configuration

--- miscellaneous/arandr_scripts/vojta/home.sh
+++ miscellaneous/arandr_scripts/vojta/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
