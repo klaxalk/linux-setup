1a38f439bf0677b173b597e859f8d70eb4cd1c7f
Vojtech Spurny <spurny@fly4future.com>
2024-11-07 11:53:53 +0100

    updated vojta's arandr scripts

--- miscellaneous/arandr_scripts/vojta/lab.sh
+++ miscellaneous/arandr_scripts/vojta/lab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
