74263de0afd3441a616ca733cd051915674f1278
Tomas Baca <klaxalk@gmail.com>
2025-01-08 10:41:04 +0100

    updated tomas's stuff

--- miscellaneous/arandr_scripts/tomas/dell_lab.sh
+++ miscellaneous/arandr_scripts/tomas/dell_lab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 3000x420 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-2-1 --off --output DP-2-1-5 --off --output DP-2-1-6 --primary --mode 1920x1080 --pos 0x645 --rotate normal --output DP-2-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-2-3 --off
