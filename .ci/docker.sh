2bd967145aed75e9243a78992b1b94cdfaf2a044
Tomas Baca <Tomas Baca>
2025-02-14 21:13:41 +0100

    building only for amd

--- .ci/docker.sh
+++ .ci/docker.sh
@@ -7,5 +7,4 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 docker login --username klaxalk --password $TOKEN
 
-docker buildx create --name container --driver=docker-container
-docker buildx build . --file Dockerfile --builder container --tag klaxalk/linux-setup:master --platform=linux/amd64,linux/arm64 --push 
+docker build . --file Dockerfile --tag klaxalk/linux-setup:master --push 
