4f1e27a91db24f5c507da23d534be962b8f3af21
stibipet <stibipet@fel.cvut.cz>
2025-02-28 19:57:24 +0100

    update petr's dmenu configs

---
+++ scripts/petr_shutdown_menu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+COLORSCHEME=$1
+
+# Options
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend='󰤄 Suspend'
+logout='󰍃 Logout'
+yes=' Yes'
+no=' No'
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-theme ~/git/linux-setup/appconfig/i3/rofi/petr-dmenu-$COLORSCHEME.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	if [[ $1 == '--shutdown' ]]; then
+		systemctl poweroff
+	elif [[ $1 == '--reboot' ]]; then
+		systemctl reboot
+	elif [[ $1 == '--suspend' ]]; then
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+	elif [[ $1 == '--logout' ]]; then
+		i3-msg exit
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+		run_cmd --suspend
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
