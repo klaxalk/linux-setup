8278bf5041eb561f8acc3af79baa2c9982ecb22d
Prochazka Ondrej <prochon4@fel.cvut.cz>
2025-11-21 13:11:13 +0100

    Adding ondra's set monitor.

---
+++ miscellaneous/arandr_scripts/ondra/dell_standalone.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-3-1 --off --output DP-1-3-2 --off --output DP-1-3-3 --off
---
+++ scripts/ondra_set_monitor_dell.sh
@@ -0,0 +1,53 @@
+PNAME=$( ps -p "$$" -o comm= )
+SNAME=$( echo "$SHELL" | grep -Eo '[^/]+/?$' )
+if [ "$PNAME" != "$SNAME" ]; then
+  exec "$SHELL" "$0" "$@"
+  exit "$?"
+else
+  source ~/."$SNAME"rc
+fi
+
+# THE NAME OF THE PROFILE SHOULD REFLECT THE NAME OF THE ARANDR FILE LATER LINKED
+
+MONITOR=$(echo "STANDALONE
+LAB" | rofi -i -dmenu -p "Select setup:")
+
+if [[ "$MONITOR" != "STANDALONE" ]] && [[ "$MONITOR" != "LAB" ]]; then
+  notify-send -u low -t 2000 "Wrong choice!" -h string:x-canonical-private-synchronous:anything
+  exit
+fi
+
+o
+
+notify-send -u low -t 2000 "Switching setup to $MONITOR" -h string:x-canonical-private-synchronous:anything
+
+# refresh the output devices
+xrandr --auto
+
+VIM_BIN="$(whereis nvim | awk '{print $2}')"
+HEADLESS="--headless"
+
+# link the arandr file
+MONITOR_LOWERCASE=$(echo $MONITOR | awk '{print tolower($0)}')
+ln -sf $GIT_PATH/linux-setup/miscellaneous/arandr_scripts/ondra/dell_$MONITOR_LOWERCASE.sh ~/.monitor.sh
+
+# change the variable in bashrc
+$VIM_BIN $HEADLESS -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" -E -s -c "%g/.*PROFILES.*MONITOR.*/norm ^/MONITORciwMONITOR_$MONITOR" -c "wqa" -- ~/."$SNAME"rc
+
+source ~/.monitor.sh
+
+# uncomment for changing the colorscheme
+source ~/."$SNAME"rc
+cd "$GIT_PATH/linux-setup"
+./backup_and_deploy.sh
+
+# reload configuration for urxvt
+xrdb ~/.Xresources
+
+# refresh the output devices
+xrandr --auto
+
+# restart i3
+i3-msg restart
+
+notify-send -u low -t 2000 "Setup switched" -h string:x-canonical-private-synchronous:anything
